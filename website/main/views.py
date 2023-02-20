23b07288d9c7d2ee0d68dfde19e0369b2a417fdd
Ilya Parshonak <parshonak@outlook.com>
2023-02-20 13:57:45 +0300

    Урок №: Работа с HTML-шаблонами. Создание стилей к сайту
    Продолжаем работать с HTML-шаблонами и на сегодняшнем занятии начнём создавать стили к нашему сайту. Выведем информацию, а также познакомимся со стилями от bootstrap.
    
    Шаблон сайта — это HTML-код с готовым дизайном и версткой для одной веб-страницы или их совокупности, который используется для построения сайта
    
    Bootstrap — это открытый и бесплатный HTML, CSS и JS фреймворк, который используется веб-разработчиками для быстрой вёрстки адаптивных дизайнов сайтов и веб-приложений.

--- website/main/views.py
+++ website/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'main/index.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'main/about.html')
