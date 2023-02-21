55321534d373dfe1b101577358cea33137ccc5be
Ilya Parshonak <parshonak@outlook.com>
2023-02-21 16:52:28 +0300

    Домашнее задание Создать новые поля для модели.

--- website/main/urls.py
+++ website/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about-us/', views.about),
-    path('profile/', views.profile),
+    path('', views.index, name= 'home'),
+    path('about-us/', views.about, name='about'),
+    path('profile/', views.profile, name= 'profile'),
 ]
