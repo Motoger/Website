ade8db06c07569e66ae376e711d5b35872dc1c66
Ilya Parshonak <parshonak@outlook.com>
2023-02-22 16:13:30 +0300

    Урок № Админ-панель. Получение и вывод данных из БД
    Продолжим изучение админ-панели, а также познакомимся с получением и выводом данных в БД. Поработаем с HTML-шаблонами, чтобы вывести информацию из базы данных.

--- website/main/models.py
+++ website/main/models.py
@@ -3,8 +3,11 @@ from django.db import models
 class Task(models.Model):
     title=models.CharField('Заголовок', max_length=50)
     task= models.TextField('Описание')
-    prise= models.IntegerField('Цена')
+    prise= models.IntegerField('Сроки')
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name="Задача"
+        verbose_name_plural='Задачи'
